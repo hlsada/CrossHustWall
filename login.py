9b23549efdd9d139c0acd12768d0470d7a0d371a
yjh2209 <45098046+hlsada@users.noreply.github.com>
2022-09-05 13:20:10 +0800

    Update login.py

--- login.py
+++ login.py
@@ -1,3 +1,4 @@
+# encoding=utf-8
 from PIL import Image
 import pytesseract
 import os
@@ -37,8 +38,8 @@ except Exception:
 
 
 time.sleep(5)
-#driver.find_element(By.CLASS_NAME, 'am-button-primary').click()
-driver.find_element_by_class_name("am-button").click()
+driver.find_element(By.NAME, '同意').click()
+#driver.find_element_by_class_name("am-button").click()
 
 time.sleep(5)
 driver.find_element(By.CLASS_NAME, 'textArea').send_keys("吃饭")
